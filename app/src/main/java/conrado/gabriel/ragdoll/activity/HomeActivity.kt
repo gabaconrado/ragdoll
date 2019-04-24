8e82e978c168540a6e20333db88fc52fdb499a82
Gabriel Conrado <gabaconrado@gmail.com>
2019-04-24 20:07:27 -0300

    Spok maçã

--- app/src/main/java/conrado/gabriel/ragdoll/activity/HomeActivity.kt
+++ app/src/main/java/conrado/gabriel/ragdoll/activity/HomeActivity.kt
@@ -1,5 +1,23 @@
 package conrado.gabriel.ragdoll.activity
 
+import android.os.Bundle
+import android.widget.Toast
 import conrado.gabriel.ragdoll.R
 
-class HomeActivity : BaseActivity(layoutId = R.layout.activity_home)
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : BaseActivity(layoutId = R.layout.activity_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        button.setOnClickListener { bt_click() }
+
+    }
+
+    private fun bt_click() {
+        textView.setText("SEJA UM VENCEDOR")
+        Toast.makeText(this, "SEJA UM VENCEDOR", Toast.LENGTH_LONG).show()
+    }
+
+}
